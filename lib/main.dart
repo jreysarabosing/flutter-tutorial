d367f705c65cea4624ad0e3b40c7d3d4c9838770
Sarabosing, J. A <j.a.sarabosing@accenture.com>
2020-11-10 18:24:55 +0800

    added message post and like system

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -22,11 +40,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = '';
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPost(String text) {
     this.setState(() {
-      this.text = text;
+      posts.add(new Post(text, 'Jonathan'));
     });
   }
 
@@ -35,8 +53,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(title: Text('Hello World!')),
         body: Column(children: <Widget>[
-          TextInputWidget(this.changeText),
-          Text(this.text)
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost)
         ]));
   }
 }
@@ -60,8 +78,9 @@ class _TextInputWidgetState extends State<TextInputWidget> {
   }
 
   void click() {
-    widget.callback(controller.text);
+    FocusScope.of(context).unfocus();
     controller.clear();
+    widget.callback(controller.text);
   }
 
   @override
@@ -77,5 +96,57 @@ class _TextInputWidgetState extends State<TextInputWidget> {
               tooltip: 'Post message',
               onPressed: this.click,
             )));
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void likeClicked(Function callback) {
+    this.setState(() {
+      callback();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  title: Text(post.body),
+                  subtitle: Text(post.author),
+                ),
+              ),
+              Row(children: <Widget>[
+                Container(
+                  child: Text(
+                    post.likes.toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                ),
+                IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () => this.likeClicked(post.likePost),
+                    color: post.userLiked ? Colors.green : Colors.black)
+              ])
+            ],
+          ),
+        );
+      },
+    );
   }
 }
